980f1ab9e6c35b201fffd40688e7e32a5a19a775
Brian Verling <117399773@umail.ucc.ie>
2018-03-06 12:20:24 +0000

    Pushing changes

--- ebusiness/ebus3.php
+++ ebusiness/ebus3.php
@@ -1,18 +1,37 @@
 <?php
-// Start the session
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>RECIEPT</title>
-        </head>
-        <body>
-            <h4>RECIEPT</h4>
-            <?php
-            // Echo session variables that were set on previous page 
-            echo "Total is " . $_SESSION["total"] . ".";
-            ?>
-        </body>
-        </html>
+        <title>RECEIPT</title>
+          <link rel="stylesheet" href="ebus.css" type="text/css" />
+    </head>
+    <body>
+          <?php  
+          $_SESSION["name"] = $_POST["name"];
+          $_SESSION["email"] = $_POST["email"];
+        ?>
+        <h4>RECEIPT</h4>
+        
+        <br>
+        <div class="ebus3name">
+        <?php
+        // Echo session variables that were set on previous page
+        echo "Name: " . $_SESSION["name"] . ".";
+        ?>
+         </div>
+         
+         <br>  
+             <?php
+        // Echo session variables that were set on previous page
+        echo "Email: " . $_SESSION["email"] . ".";
+        ?>
+        
+        <br>
+             <?php
+        // Echo session variables that were set on previous page
+        echo "Total: $" . $_SESSION["total"] . ".";
+        ?>
+    </body>
+</html>
